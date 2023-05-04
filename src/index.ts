8400144a5b6a9ae3476a953b202509c03b4d2985
DuNT <trongdu94@gmail.com>
2023-05-04 21:54:36 +0700

    refactor FileReader using interface

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// create an object that satisfies DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// create instance of MatchReader and pass in sth satisfying the DataReader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let muWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     muWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
